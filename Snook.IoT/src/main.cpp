f688b876b616f59f74601e78a173de062d5f9ab8
segelfartyg <samuel.swaren@outlook.com>
2023-11-16 21:35:04 +0100

    Add: functionality for Adafruit display in iot proj

--- Snook.IoT/src/main.cpp
+++ Snook.IoT/src/main.cpp
@@ -13,164 +13,563 @@ const char *password_router = SECRET_WIFI_PASSWORD;
 const char *server = SERVER_IP;
 const char *deviceId = SERVER_IP;
 
-WiFiClient httpclient;
-WebSocketsClient webSocket;
 
-bool i2CAddrTest(uint8_t addr)
-{
 
-  Wire.begin();
-  Wire.beginTransmission(addr);
-  if (Wire.endTransmission() == 0)
-  {
-    return true;
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void testdrawline() {
+  int16_t i;
+
+  display.clearDisplay(); // Clear display buffer
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, WHITE);
+    display.display(); // Update screen with each newly-drawn line
+    delay(1);
   }
-  return false;
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  delay(250);
+
+  display.clearDisplay();
+
+  for(i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, WHITE);
+    display.display();
+    delay(1);
+  }
+  for(i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000); // Pause for 2 seconds
 }
 
-void setupWifi()
-{
+void testdrawrect(void) {
+  display.clearDisplay();
 
-  WiFi.disconnect();
-
-  Serial.println("BEGINNING WIFI CONNECTION");
-  WiFi.begin(ssid_router, password_router);
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.println(WiFi.status());
-    Serial.println("Connecting...");
-    delay(500);
+  for(int16_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
+    display.display(); // Update screen with each newly-drawn rectangle
+    delay(1);
   }
-  Serial.println("CONNECTED IP:");
-  Serial.println(WiFi.localIP());
+
+  delay(2000);
 }
 
-String dataReader(WiFiClient _httpclient)
-{
+void testfillrect(void) {
+  display.clearDisplay();
 
-  String response;
-  bool responseReceived = false;
-  bool timeOut = false;
-  int retries = 0;
+  for(int16_t i=0; i<display.height()/2; i+=3) {
+    // The INVERSE color is used so rectangles alternate white/black
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, INVERSE);
+    display.display(); // Update screen with each newly-drawn rectangle
+    delay(1);
+  }
 
-  while (!responseReceived && !timeOut)
-  {
+  delay(2000);
+}
 
-    while (_httpclient.available())
-    {
-      char c = _httpclient.read();
-      response += c;
+void testdrawcircle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testfillcircle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=3) {
+    // The INVERSE color is used so circles alternate white/black
+    display.fillCircle(display.width() / 2, display.height() / 2, i, INVERSE);
+    display.display(); // Update screen with each newly-drawn circle
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testdrawroundrect(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
+      display.height()/4, WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testfillroundrect(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<display.height()/2-2; i+=2) {
+    // The INVERSE color is used so round-rects alternate white/black
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
+      display.height()/4, INVERSE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testdrawtriangle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(
+      display.width()/2  , display.height()/2-i,
+      display.width()/2-i, display.height()/2+i,
+      display.width()/2+i, display.height()/2+i, WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testfilltriangle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
+    // The INVERSE color is used so triangles alternate white/black
+    display.fillTriangle(
+      display.width()/2  , display.height()/2-i,
+      display.width()/2-i, display.height()/2+i,
+      display.width()/2+i, display.height()/2+i, INVERSE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+void testdrawchar(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw white text
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  // Not all the characters will fit on the display. This is normal.
+  // Library will draw what it can and the rest will be clipped.
+  for(int16_t i=0; i<256; i++) {
+    if(i == '\n') display.write(' ');
+    else          display.write(i);
+  }
+
+  display.display();
+  delay(2000);
+}
+
+void testdrawstyles(void) {
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.println(F("Hello, world!"));
+
+  display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
+  display.println(3.141592);
+
+  display.setTextSize(2);             // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.print(F("0x")); display.println(0xDEADBEEF, HEX);
+
+  display.display();
+  delay(2000);
+}
+
+void testscrolltext(void) {
+  display.clearDisplay();
+
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(WHITE);
+  display.setCursor(10, 0);
+  display.println(F("scroll"));
+  display.display();      // Show initial text
+  delay(100);
+
+  // Scroll in various directions, pausing in-between:
+  display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+}
+
+void testdrawbitmap(void) {
+  display.clearDisplay();
+
+  display.drawBitmap(
+    (display.width()  - LOGO_WIDTH ) / 2,
+    (display.height() - LOGO_HEIGHT) / 2,
+    logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, 1);
+  display.display();
+  delay(1000);
+}
+
+#define XPOS   0 // Indexes into the 'icons' array in function below
+#define YPOS   1
+#define DELTAY 2
+
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, icons[NUMFLAKES][3];
+
+  // Initialize 'snowflake' positions
+  for(f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+    icons[f][YPOS]   = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+
+  for(;;) { // Loop forever...
+    display.clearDisplay(); // Clear the display buffer
+
+    // Draw each snowflake:
+    for(f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], bitmap, w, h, WHITE);
     }
 
-    if (response.length() > 0)
-    {
-      responseReceived = true;
-    }
-    else
-    {
-      if (retries < 5)
-      {
-        delay(1000);
-        retries++;
+    display.display(); // Show the display buffer on the screen
+    delay(200);        // Pause for 1/10 second
+
+    // Then update coordinates of each flake...
+    for(f=0; f< NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
       }
-      else
-      {
-        response = "INTERNAL READING ERROR (001)";
-        ESP.restart();
-        return response;
-      }
     }
   }
-
-  Serial.println(response);
-  return response;
-}
-
-void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
-	const uint8_t* src = (const uint8_t*) mem;
-	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
-	for(uint32_t i = 0; i < len; i++) {
-		if(i % cols == 0) {
-			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
-		}
-		Serial.printf("%02X ", *src);
-		src++;
-	}
-	Serial.printf("\n");
-}
-
-void showLCDMessage(uint8_t * message){
-
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(String((char *)message));
-
-}
-
-void webSocketEvent(WStype_t type, uint8_t * payload, size_t length){
-
-	switch(type) {
-		case WStype_DISCONNECTED:
-			Serial.println("[WSc] Disconnected!\n");
-			break;
-		case WStype_CONNECTED:
-			Serial.printf("[WSc] Connected to url: %s\n", payload);
-
-			// send message to server when Connected
-			webSocket.sendTXT("Connected");
-			break;
-		case WStype_TEXT:
-			Serial.printf("[WSc] get text: %s\n", payload);
-      showLCDMessage(payload);
-
-			// send message to server
-			// webSocket.sendTXT("message here");
-			break;
-		case WStype_BIN:
-			Serial.printf("[WSc] get binary length: %u\n", length);
-			hexdump(payload, length);
-
-			// send data to server
-			// webSocket.sendBIN(payload, length);
-			break;
-		case WStype_ERROR:			
-		case WStype_FRAGMENT_TEXT_START:
-		case WStype_FRAGMENT_BIN_START:
-		case WStype_FRAGMENT:
-		case WStype_FRAGMENT_FIN:
-			break;
-	}
-
 }
 
 
-void setup()
-{
+void setup() {
+  Serial.begin(115200);
 
-  webSocket.begin(server, 80, "/ws");
-
-  webSocket.onEvent(webSocketEvent);
-
-  // try ever 5000 again if connection has failed
-	webSocket.setReconnectInterval(5000);
-
-
-  Serial.begin(9600);
-
-  if (!i2CAddrTest(0x27))
-  {
-    lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
   }
 
-  setupWifi();
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
 
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  // display.display() is NOT necessary after every single drawing command,
+  // unless that's what you want...rather, you can batch up a bunch of
+  // drawing operations and then update the screen all at once by calling
+  // display.display(). These examples demonstrate both approaches...
+
+
+
+  testdrawline();      // Draw many lines
+
+  testdrawrect();      // Draw rectangles (outlines)
+
+  testfillrect();      // Draw rectangles (filled)
+
+  testdrawcircle();    // Draw circles (outlines)
+
+  testfillcircle();    // Draw circles (filled)
+
+  testdrawroundrect(); // Draw rounded rectangles (outlines)
+
+  testfillroundrect(); // Draw rounded rectangles (filled)
+
+  testdrawtriangle();  // Draw triangles (outlines)
+
+  testfilltriangle();  // Draw triangles (filled)
+
+  testdrawchar();      // Draw characters of the default font
+
+  testdrawstyles();    // Draw 'stylized' characters
+
+  testscrolltext();    // Draw scrolling text
+
+  testdrawbitmap();    // Draw a small bitmap image
+
+  // Invert and restore display, pausing in-between
+  display.invertDisplay(true);
+  delay(1000);
+  display.invertDisplay(false);
+  delay(1000);
+
+  testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
 }
 
-void loop()
-{
-  webSocket.loop();
+void loop() {
 }
+
+
+
+
+// WiFiClient httpclient;
+// WebSocketsClient webSocket;
+
+// bool i2CAddrTest(uint8_t addr)
+// {
+
+//   Wire.begin();
+//   Wire.beginTransmission(addr);
+//   if (Wire.endTransmission() == 0)
+//   {
+//     return true;
+//   }
+//   return false;
+// }
+
+// void setupWifi()
+// {
+
+//   WiFi.disconnect();
+
+//   Serial.println("BEGINNING WIFI CONNECTION");
+//   WiFi.begin(ssid_router, password_router);
+
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     Serial.println(WiFi.status());
+//     Serial.println("Connecting...");
+//     delay(500);
+//   }
+//   Serial.println("CONNECTED IP:");
+//   Serial.println(WiFi.localIP());
+// }
+
+// String dataReader(WiFiClient _httpclient)
+// {
+
+//   String response;
+//   bool responseReceived = false;
+//   bool timeOut = false;
+//   int retries = 0;
+
+//   while (!responseReceived && !timeOut)
+//   {
+
+//     while (_httpclient.available())
+//     {
+//       char c = _httpclient.read();
+//       response += c;
+//     }
+
+//     if (response.length() > 0)
+//     {
+//       responseReceived = true;
+//     }
+//     else
+//     {
+//       if (retries < 5)
+//       {
+//         delay(1000);
+//         retries++;
+//       }
+//       else
+//       {
+//         response = "INTERNAL READING ERROR (001)";
+//         ESP.restart();
+//         return response;
+//       }
+//     }
+//   }
+
+//   Serial.println(response);
+//   return response;
+// }
+
+// void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+// 	const uint8_t* src = (const uint8_t*) mem;
+// 	Serial.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+// 	for(uint32_t i = 0; i < len; i++) {
+// 		if(i % cols == 0) {
+// 			Serial.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+// 		}
+// 		Serial.printf("%02X ", *src);
+// 		src++;
+// 	}
+// 	Serial.printf("\n");
+// }
+
+// void showLCDMessage(uint8_t * message){
+
+//   lcd.clear();
+//   lcd.setCursor(0, 0);
+//   lcd.print(String((char *)message));
+
+// }
+
+// void webSocketEvent(WStype_t type, uint8_t * payload, size_t length){
+
+// 	switch(type) {
+// 		case WStype_DISCONNECTED:
+// 			Serial.println("[WSc] Disconnected!\n");
+// 			break;
+// 		case WStype_CONNECTED:
+// 			Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+// 			// send message to server when Connected
+// 			webSocket.sendTXT("Connected");
+// 			break;
+// 		case WStype_TEXT:
+// 			Serial.printf("[WSc] get text: %s\n", payload);
+//       showLCDMessage(payload);
+
+// 			// send message to server
+// 			// webSocket.sendTXT("message here");
+// 			break;
+// 		case WStype_BIN:
+// 			Serial.printf("[WSc] get binary length: %u\n", length);
+// 			hexdump(payload, length);
+
+// 			// send data to server
+// 			// webSocket.sendBIN(payload, length);
+// 			break;
+// 		case WStype_ERROR:			
+// 		case WStype_FRAGMENT_TEXT_START:
+// 		case WStype_FRAGMENT_BIN_START:
+// 		case WStype_FRAGMENT:
+// 		case WStype_FRAGMENT_FIN:
+// 			break;
+// 	}
+
+// }
+
+
+// void setup()
+// {
+
+//   webSocket.begin(server, 80, "/ws");
+
+//   webSocket.onEvent(webSocketEvent);
+
+//   // try ever 5000 again if connection has failed
+// 	webSocket.setReconnectInterval(5000);
+
+
+//   Serial.begin(9600);
+
+//   if (!i2CAddrTest(0x27))
+//   {
+//     lcd = LiquidCrystal_I2C(0x3F, 16, 2);
+//   }
+
+//   setupWifi();
+
+//   lcd.init();
+//   lcd.backlight();
+//   lcd.setCursor(0, 0);
+// }
+
+// void loop()
+// {
+//   webSocket.loop();
+// }
